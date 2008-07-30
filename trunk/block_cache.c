cb587f18f5493b365353c9223fa887086bda1896
archie.cobbs <archie.cobbs@0a1e4853-1050-0410-bd56-115aef1bd559>
2008-07-30 01:17:31 +0000

    Very small optimzation.

--- trunk/block_cache.c
+++ trunk/block_cache.c
@@ -488,8 +488,8 @@ block_cache_worker_main(void *arg)
 
             /* If that failed, go back to the DIRTY state and try again */
             if (r != 0) {
-                TAILQ_INSERT_TAIL(&priv->dirties, entry, link);
                 ENTRY_SET_DIRTY(entry);
+                TAILQ_INSERT_TAIL(&priv->dirties, entry, link);
                 continue;
             }
 
