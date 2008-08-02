fff006c1f82acb8ffe900b6cd776b361c1fe113c
archie.cobbs <archie.cobbs@0a1e4853-1050-0410-bd56-115aef1bd559>
2008-08-02 21:09:24 +0000

    Make test program more rigorous.

--- trunk/tester.c
+++ trunk/tester.c
@@ -31,63 +31,66 @@
 
 /* Definitions */
 #define NUM_THREADS     10
-#define NUM_LOCKS       25
-#define DELAY_BASE      500
-#define DELAY_RANGE     150
+#define DELAY_BASE      100
+#define DELAY_RANGE     500
 #define READ_FACTOR     2
 #define ZERO_FACTOR     3
 
+/* Block states */
+struct block_state {
+    u_int               writing;        // block is currently being written by a thread
+    u_int               counter;        // counts writes to the block
+    u_int               content;        // most recently written content
+};
+
 /* Internal functions */
 static void *thread_main(void *arg);
+static void logit(int id, const char *fmt, ...) __attribute__ ((__format__ (__printf__, 2, 3)));
+static uint64_t get_time(void);
 
 /* Internal variables */
-static u_char *md5s;
 static void *zero_block;
-static u_char zero_md5[MD5_DIGEST_LENGTH];
-static pthread_mutex_t locks[NUM_LOCKS];
+static pthread_mutex_t mutex;
 static struct s3b_config *config;
 static struct s3backer_store *store;
+static struct block_state *blocks;
+static uint64_t start_time;
 
 int
 main(int argc, char **argv)
 {
     pthread_t thread;
-    MD5_CTX ctx;
     int i;
     int r;
 
     /* Get configuration */
     if ((config = s3backer_get_config(argc, argv)) == NULL)
         exit(1);
+    if (config->block_size < sizeof(u_int))
+        err(1, "block size too small");
 
     /* Open store */
     if ((store = s3backer_create_store(config)) == NULL)
         err(1, "s3backer_create_store");
 
-    /* Compute MD5 of zero block */
+    /* Allocate block states */
+    if ((blocks = calloc(config->num_blocks, sizeof(*blocks))) == NULL)
+        err(1, "calloc");
+
+    /* Create zero block */
     if ((zero_block = calloc(1, config->block_size)) == NULL)
         err(1, "calloc");
-    MD5_Init(&ctx);
-    MD5_Update(&ctx, zero_block, config->block_size);
-    MD5_Final(zero_md5, &ctx);
-    fprintf(stderr, "ZERO: %02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x\n",
-      zero_md5[0], zero_md5[1], zero_md5[2], zero_md5[3], zero_md5[4], zero_md5[5], zero_md5[6], zero_md5[7],
-      zero_md5[8], zero_md5[9], zero_md5[10], zero_md5[11], zero_md5[12], zero_md5[13], zero_md5[14], zero_md5[15]);
 
     /* Random initialization */
     srandom((u_int)time(NULL));
-    for (i = 0; i < NUM_LOCKS; i++)
-        pthread_mutex_init(&locks[i], NULL);
-    if ((md5s = malloc(config->num_blocks * MD5_DIGEST_LENGTH)) == NULL)
-        err(1, "malloc");
-    for (i = 0; i < config->num_blocks; i++)
-        memcpy(&md5s[i * MD5_DIGEST_LENGTH], zero_md5, MD5_DIGEST_LENGTH);
+    pthread_mutex_init(&mutex, NULL);
+    start_time = get_time();
 
     /* Zero all blocks */
     for (i = 0; i < config->num_blocks; i++) {
-        fprintf(stderr, "zeroing block #%u\n", i);
+        printf("zeroing block %08x\n", i);
         if ((r = (*store->write_block)(store, i, zero_block, NULL)) != 0)
-            warnx("write error: %s", strerror(r));
+            err(1, "write error");
     }
 
     /* Create threads */
@@ -105,11 +108,8 @@ thread_main(void *arg)
     const int id = (int)arg;
     struct timespec delay;
     u_char data[config->block_size];
-    u_char md5[MD5_DIGEST_LENGTH];
     s3b_block_t block_num;
-    MD5_CTX ctx;
     int millis;
-    int lockid;
     int r;
 
     /* Loop */
@@ -123,44 +123,92 @@ thread_main(void *arg)
 
         // Pick a random block
         block_num = random() % config->num_blocks;
-        lockid = block_num % NUM_LOCKS;
 
-        // Randomly read or write
-        pthread_mutex_lock(&locks[lockid]);
+        // Randomly read or write it
         if ((random() % READ_FACTOR) != 0) {
-            fprintf(stderr, "[%2d] rd #%u START\n", id, block_num);
+            struct block_state *const state = &blocks[block_num];
+            struct block_state before;
+            struct block_state after;
+
+            // Snapshot block state
+            pthread_mutex_lock(&mutex);
+            memcpy(&before, state, sizeof(before));
+            pthread_mutex_unlock(&mutex);
+
+            // Do the read
+            logit(id, "rd %08x START\n", block_num);
             if ((r = (*store->read_block)(store, block_num, data, NULL)) != 0) {
-                warnx("[%2d] read error: %s", id, strerror(r));
+                logit(id, "****** READ ERROR: %s", strerror(r));
                 continue;
             }
-            MD5_Init(&ctx);
-            MD5_Update(&ctx, data, config->block_size);
-            MD5_Final(md5, &ctx);
-            if (memcmp(md5, &md5s[block_num * MD5_DIGEST_LENGTH], MD5_DIGEST_LENGTH) != 0) {
-                const u_char *g = md5;
-                const u_char *e = &md5s[block_num * MD5_DIGEST_LENGTH];
 
-                fprintf(stderr, "[%2d] EXP: %02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x\n",
-                  id, e[0], e[1], e[2], e[3], e[4], e[5], e[6], e[7], e[8], e[9], e[10], e[11], e[12], e[13], e[14], e[15]);
-                fprintf(stderr, "[%2d] GOT: %02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x\n",
-                  id, g[0], g[1], g[2], g[3], g[4], g[5], g[6], g[7], g[8], g[9], g[10], g[11], g[12], g[13], g[14], g[15]);
-                errx(1, "got wrong MD5 block #%u", block_num);
+            // Snapshot block state again
+            pthread_mutex_lock(&mutex);
+            memcpy(&after, state, sizeof(before));
+            pthread_mutex_unlock(&mutex);
+
+            // Verify content, but only if no write occurred while we were reading
+            if (before.writing == 0 && after.writing == 0 && before.counter == after.counter) {
+                if (memcmp(data, &before.content, sizeof(before.content)) != 0) {
+                    logit(id, "got wrong content block %08x", block_num);
+                    exit(1);
+                }
             }
-            fprintf(stderr, "[%2d] rd #%u COMPLETE\n", id, block_num);
+            logit(id, "rd %08x content=0x%08x COMPLETE\n", block_num, *(u_int *)data);
         } else {
-            *((u_int *)data) = random() % ZERO_FACTOR != 0 ? 0 : (u_int)random();
-            fprintf(stderr, "[%2d] wr #%u 0x%08x START\n", id, block_num, *(u_int *)data);
-            MD5_Init(&ctx);
-            MD5_Update(&ctx, data, config->block_size);
-            MD5_Final(md5, &ctx);
+            struct block_state *const state = &blocks[block_num];
+            u_int content;
+
+            // Update block state
+            pthread_mutex_lock(&mutex);
+            if (state->writing) {                   // only one writer at a time
+                pthread_mutex_unlock(&mutex);
+                continue;
+            }
+            state->writing = 1;
+            pthread_mutex_unlock(&mutex);
+
+            // Write block
+            content = (random() % ZERO_FACTOR) != 0 ? 0 : (u_int)random();
+            memcpy(data, &content, sizeof(content));
+            memset(data + sizeof(content), 0, config->block_size - sizeof(content));
+            logit(id, "wr %08x content=0x%08x START\n", block_num, *(u_int *)data);
             if ((r = (*store->write_block)(store, block_num, data, NULL)) != 0)
-                warnx("[%2d] write error: %s", id, strerror(r));
-            else
-                memcpy(&md5s[block_num * MD5_DIGEST_LENGTH], md5, MD5_DIGEST_LENGTH);
-            fprintf(stderr, "[%2d] wr #%u 0x%08x %s%s\n", id, block_num, *(u_int *)data,
-              r != 0 ? "FAILED" : "COMPLETE", r != 0 ? strerror(r) : "");
+                logit(id, "****** WRITE ERROR: %s", strerror(r));
+            logit(id, "wr %08x content=0x%08x %s%s\n", block_num, *(u_int *)data,
+              r != 0 ? "FAILED: " : "COMPLETE", r != 0 ? strerror(r) : "");
+
+            // Update block state
+            pthread_mutex_lock(&mutex);
+            if (r == 0) {
+                state->counter++;
+                state->content = content;
+            }
+            state->writing = 0;
+            pthread_mutex_unlock(&mutex);
         }
-        pthread_mutex_unlock(&locks[lockid]);
     }
+}
+
+static void
+logit(int id, const char *fmt, ...)
+{
+    uint64_t timestamp = get_time() - start_time;
+    va_list args;
+
+    printf("%u.%03u [%02d] ", (u_int)(timestamp / 1000), (u_int)(timestamp % 1000), id);
+    va_start(args, fmt);
+    vfprintf(stdout, fmt, args);
+    va_end(args);
+}
+
+
+static uint64_t
+get_time(void)
+{
+    struct timeval tv;
+
+    gettimeofday(&tv, NULL);
+    return (uint64_t)tv.tv_sec * 1000 + (uint64_t)tv.tv_usec / 1000;
 }
 
