5ab104875ab1ffa55faf0145108c99d26d98e2da
Archie L. Cobbs <archiecobbs@users.noreply.github.com>
2008-08-04 21:45:27 +0000

    Cleanup config.h.in~

--- cleanup.sh
+++ cleanup.sh
@@ -9,7 +9,7 @@ set -e
 
 echo "cleaning up"
 rm -rf autom4te*.cache scripts aclocal.m4 configure config.log config.status .deps stamp-h1
-rm -f config.h.in config.h
+rm -f config.h.in config.h.in~ config.h
 rm -f scripts
 rm -f Makefile Makefile.in
 rm -f svnrev.c
