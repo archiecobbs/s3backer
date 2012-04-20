a5f163d07fea38e4792def5b7482d3a0d93d3f1c
archie.cobbs <archie.cobbs@0a1e4853-1050-0410-bd56-115aef1bd559>
2012-04-20 14:47:06 +0000

    Format man page with narrower columns so it doesn't get chopped.

--- wiki/manpage.sh
+++ wiki/manpage.sh
@@ -7,7 +7,7 @@ cat << "xxEOFxx"
 {{{
 xxEOFxx
 
-groff -r LL=131n -r LT=131n -Tlatin1 -man ../trunk/s3backer.1 | sed -r -e 's/.\x08(.)/\1/g' -e 's/[[0-9]+m//g' 
+groff -r LL=100n -r LT=100n -Tlatin1 -man ../trunk/s3backer.1 | sed -r -e 's/.\x08(.)/\1/g' -e 's/[[0-9]+m//g' 
 
 cat << "xxEOFxx"
 }}}
