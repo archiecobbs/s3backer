c9afea32b6a2e87e7bbdd97aee669072e29a29fb
Archie L. Cobbs <archie.cobbs@gmail.com>
2022-04-26 13:12:13 -0500

    Fix bug in previous commit.

--- cleanup.sh
+++ cleanup.sh
@@ -10,7 +10,7 @@ echo "cleaning up"
 rm -rf autom4te*.cache scripts aclocal.m4 configure config.log config.status .deps stamp-h1
 rm -f config.h.in config.h.in~ config.h
 rm -f .libs *.lo *.la libtool
-rm -f scripts m4 TAGS
+rm -rf scripts m4 TAGS
 find . \( -name Makefile -o -name Makefile.in \) -print0 | xargs -0 rm -f
 rm -f gitrev.c s3backer.spec
 rm -f *.o s3backer tester
